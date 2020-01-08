d95376feaad15c12ee589f4bb5b82d0759af8a21
Thomas Alrek <thomas@alrek.no>
2020-01-08 22:55:11 +0100

    Auto generate middleware groups

--- src/Routing/RoutingServiceProvider.php
+++ src/Routing/RoutingServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Netflex\Routing;
 
 use Illuminate\Routing\RoutingServiceProvider as ServiceProvider;
+use Netflex\Builder\Page;
 
 class RoutingServiceProvider extends ServiceProvider
 {
@@ -16,5 +17,19 @@ class RoutingServiceProvider extends ServiceProvider
     $this->app->singleton('router', function ($app) {
       return new Router($app['events'], $app);
     });
+  }
+
+  public function boot()
+  {
+    // Inject a middleware group for handling Netflex pages
+    $router = $this->app->make('router');
+
+    $router->middlewareGroup('page', [
+      \Netflex\Http\Middleware\BindPage::class,
+    ]);
+
+    $router->middlewareGroup('group_auth', [
+      \Netflex\Http\Middleware\GroupAuthentication::class,
+    ]);
   }
 }
