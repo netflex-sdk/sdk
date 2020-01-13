48943a415ea5c29c2138b1182ce87d9a5e42e12b
Thomas Alrek <thomas@alrek.no>
2020-01-13 18:15:26 +0100

    Use helper method to bind page

--- src/Http/Middleware/BindPage.php
+++ src/Http/Middleware/BindPage.php
@@ -18,14 +18,7 @@ class BindPage
   public function handle($request, Closure $next)
   {
     if (get_class($request->route()) === Route::class) {
-      if (!app()->has('page')) {
-
-        // Bind the matched page into the container for later use
-        // This is used internally in Page::current() to resolve the current page
-        app()->bind('page', function () use ($request) {
-          return $request->route()->data('page');
-        });
-      }
+      current_page($request->route()->data('page'));
     }
 
     return $next($request);
