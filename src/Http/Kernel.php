3684cfe4b11787322c2c6d3f3b5b746431ba9219
Thomas Alrek <thomas@alrek.no>
2020-05-25 20:52:46 +0200

    Add hooks to inject custom domains into Http Kernel

---
+++ src/Http/Kernel.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Netflex\Http;
+
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Facade;
+
+use Illuminate\Routing\Pipeline;
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+  /**
+   * @param Request $request
+   * @param string $domain
+   * @return Request
+   */
+  protected function injectDomain($request, $domain)
+  {
+    $original = $request->headers->get('host', '');
+    $request->headers->set('host', $domain);
+    URL::forceRootUrl($request->getScheme() . '://' . $original);
+
+    return $request;
+  }
+
+  /**
+   * @param  \Illuminate\Http\Request  $request
+   * @return Request
+   */
+  protected function modifyRequest($request)
+  {
+    if ($this->app->environment() !== 'master') {
+      if ($domains = $this->app['config']->get('domains')) {
+        $original = $request->headers->get('host', '');
+        list($host) = explode(':', $original);
+
+        if (array_key_exists($host, $domains['mappings'] ?? [])) {
+          return $this->injectDomain($request, $domains['mappings'][$host]);
+        }
+
+        if (isset($domains['default'])) {
+          return $this->injectDomain($request, $domains['default']);
+        }
+      };
+    }
+
+    return $request;
+  }
+
+  /**
+   * Send the given request through the middleware / router.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  protected function sendRequestThroughRouter($request)
+  {
+    // Inject Request so we are able to bootstrap
+    $this->app->instance('request', $request);
+    $this->bootstrap();
+
+    // Replace injected Request
+    $this->app->instance('request', $this->modifyRequest($request));
+
+    Facade::clearResolvedInstance('request');
+
+    return (new Pipeline($this->app))
+      ->send($request)
+      ->through($this->app->shouldSkipMiddleware() ? [] : $this->middleware)
+      ->then($this->dispatchToRouter());
+  }
+}
